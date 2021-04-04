fcc3089e4023c4c0b5e401768942cce75cd89ca2
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-04-04 22:35:24 +0530

    sticking navigation bar on scroll

--- js/script.js
+++ js/script.js
@@ -90,4 +90,16 @@ $(document).ready(function () {
 
         return false;
     });
+
+    const nav = $('#navigation');
+    const navTop = nav.offset().top;
+    $(window).on('scroll', stickyNavigation);
+    function stickyNavigation() {
+        const body = $('body');
+        if ($(window).scrollTop() >= navTop) {
+            body.addClass('fixedNav');
+        } else {
+            body.removeClass('fixedNav');
+        }
+    }
 });
