f541d1eb7defea94bb4b7d590ba55df1efc91330
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-04-04 12:53:24 +0530

    styling the items

--- js/script.js
+++ js/script.js
@@ -64,4 +64,13 @@ $(document).ready(function () {
     });
 
     $('[data-fancybox]').fancybox();
+
+    $('.items').isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    });
 });
