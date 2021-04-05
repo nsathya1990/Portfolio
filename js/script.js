1a0c80d89542e2a745eb7857921574b8a4cabb9b
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-04-06 00:19:22 +0530

    mobile navbar dropdown fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(window).on('load', function () {
-    $('.loader .inner').fadeOut(500, function () {
-        $('.loader').fadeOut(750);
+    $('.loader .inner').fadeOut(100, function () {
+        $('.loader').fadeOut(250);
     });
 
     $('.items').isotope({
@@ -97,13 +97,13 @@ $(document).ready(function () {
         return false;
     });
 
-    /* $('#navigation li a').click(function (e) {
+    $('#navigation li a').click(function (e) {
         e.preventDefault();
 
         const targetElement = $(this).attr('href');
         const targetPosition = $(targetElement).offset().top;
         $('html, body').animate({ scrollTop: targetPosition - 50 }, 'slow');
-    }); */
+    });
 
     const nav = $('#navigation');
     const navTop = nav.offset().top;
